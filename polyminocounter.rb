f52f12b1db51a9c25921dcf8f5ea8d3185889391
Gadi Aleksandrowicz <gadial@gadial-desktop.(none)>
2008-07-31 19:30:21 +0300

    All bugs fixed, but slow as hell

--- polyminocounter.rb
+++ polyminocounter.rb
@@ -1,3 +1,9 @@
+class Array
+	def count_item(x)
+		self.inject(0){|sum, item| x==item ? sum+1 : sum}
+	end
+end
+
 class Square
 	include Comparable
 	attr_accessor :coords
@@ -20,6 +26,12 @@ class Square
 	def hash
 		return self.coords.hash
 	end
+	def eql?(other)
+		return self.coords==other.coords
+	end
+	def ==(other)
+		return self.coords==other.coords
+	end
 	def <=> (other)
 		self.coords <=> other.coords
 	end
@@ -29,6 +41,7 @@ class Square
 end
 
 class Grid
+	attr_reader :squares
 	def initialize(dimensions)
 		@dimensions=dimensions
 		@squares=[]
@@ -40,20 +53,23 @@ class Grid
 		@squares << square
 		@squares.uniq!
 	end
-
+	def remove_square(square)
+		squares.delete(square)
+	end
 	def new_neighbors(square)
 		#neighbors of square that are not neighbors of any other polyomino square
 		#assumes square is not yet in the polyomino
-		old_neighbors=@squares.collect{|s| s.neighbors}.uniq
-		return square.neighbors.reject{|s| old_neighbors.include?(s)}.reject{|s| s<self.origin}
+		old_neighbors=@squares.collect{|s| s.neighbors}.flatten.uniq
+		return square.neighbors.reject{|s| old_neighbors.include?(s) or @squares.include?(s) or s<self.origin}
 	end
 end
 
 class RedelmeierAlgorithm
-	attr_accessor :n, :d, :grid, :count
+	attr_accessor :n, :d, :grid, :count, :polyominoes, :counts_tree_polyominoes
 	def initialize(n,d)
 		self.n=n
 		self.d=d
+		self.counts_tree_polyominoes=false
 	end
 
 	def add_square(untried_set,new_square)
@@ -61,6 +77,7 @@ class RedelmeierAlgorithm
 		new_neighbors=self.grid.new_neighbors(new_square)
 		new_untried_set+=new_neighbors
 		self.grid << new_square
+		new_untried_set.reject!{|s| self.grid.squares.collect{|x| x.neighbors}.flatten.count_item(s)>1} if self.counts_tree_polyominoes
 		return new_untried_set
 	end
 
@@ -68,23 +85,31 @@ class RedelmeierAlgorithm
                 self.grid=Grid.new(self.d)
                 untried_set=[grid.origin]
                 self.count=[0]*self.n
+		self.polyominoes=[]
+		n.times {|i| self.polyominoes[i]=[]}
                 recurse(1,untried_set)
 		return self
 	end
 
 	def recurse(current_size,untried_set)
-# 		puts "new recursion with untried_set: #{untried_set.inspect}"
+#   		puts "new recursion with untried_set: #{untried_set.inspect}"
 		while not untried_set.empty?
 			new_square=untried_set.pop
+#  			puts "current polyomino: #{self.grid.squares.inspect}, adding: #{new_square.inspect}"
 			new_untried_set=add_square(untried_set,new_square)
 			self.count[current_size-1]+=1
+			self.polyominoes[current_size-1] << self.grid.squares.dup
 			recurse(current_size+1,new_untried_set) unless current_size>=self.n
+			self.grid.remove_square(new_square)
 		end
 	end
 	def print_results
+#		puts self.polyominoes[3].inspect
 		puts count.inspect
 	end
 end
 
-test=RedelmeierAlgorithm.new(6,2)
+test=RedelmeierAlgorithm.new(10,3)
+test.counts_tree_polyominoes=true
 test.run.print_results
+
